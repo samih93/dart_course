d39528a3332fdb6172be192d1ec2a71091ddc19c
samih93 <samih_damaj@hotmail.com>
2023-04-17 04:10:27 +0300

    1 and 2

--- bin/1- type of variable.dart
+++ bin/1- type of variable.dart
@@ -1,4 +1,9 @@
 // Type of variable
-//NOTE int ,double ,string ,bool
+//NOTE int ,double ,string ,bool 
 
-void main(List<String> args) {}
+void main(List<String> args) {
+  double a = 2.5;
+  String asss = '';
+  bool Isactive = true;
+  bool IsActive = false;
+}
--- bin/2 - parsing.dart
+++ bin/2 - parsing.dart
@@ -4,12 +4,68 @@ void main(List<String> args) {
   // !.toString() applied to int , string ,double .... ,
   // ! as String if u dont know the type and result should be string
 
+//  assign int to String
   var a = 4;
   print(a);
   String b;
   b = a.toString();
 
-  String j = "samih";
-  var res = int.tryParse(j);
-  print(res);
+  // assign string to in
+  int c = int.parse(b);
+
+// try parse for integer
+  String field = "4abc";
+  int? value = int.tryParse(field); // value => null
+
+  String field1 = "4";
+  int? value1 = int.tryParse(field); // value1 => 4
+
+  if (value != null) {
+    // value is integer
+    // post data to database
+  } else {
+    // value is string
+    // thro Exeption ;
+  }
+
+  // try parse for double
+  String va = "4";
+  double res3 = double.parse(va);
+  print("res 3 ${res3}");
+
+  int va1 = 4;
+  var res4 = double.tryParse(va1.toString());
+  print("res 4 ${res4}");
+
+  // as double , as int ,as String
+  dynamic o = "4.1";
+  o = 4.1;
+  // var ht = o as String;
+  // var ht1 = o as double;
+  // var ht2 = o as int;
+
+  double? l;
+  double newval = l != null ? l : 0;
+  print(newval);
+
+// l != null ? l : 0;
+  if (l != null && o != null) {
+    newval = l;
+  } else {
+    newval = 0;
+  }
+
+  String? username;
+  print(username != null ? username : "no info");
+  print(username ?? "no info");
+
+  double note = 15;
+  // >15 excelant
+  // >=10 good
+  // <10 failed
+  print(note < 10
+      ? "failed"
+      : note < 15
+          ? "good"
+          : "excelant");
 }
