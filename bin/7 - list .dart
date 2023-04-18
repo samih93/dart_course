851456da48f46e98eb64229751e0d0622d4c117f
samih93 <samih_damaj@hotmail.com>
2023-04-18 12:56:09 +0300

    ssad

--- bin/7 - list .dart
+++ bin/7 - list .dart
@@ -1,8 +1,10 @@
 void main(List<String> args) {
   List list = ["samih", "ahmad", "damaj"];
-  List list1;
+  List list1 = [];
   List<int> list2 = [];
 
   // not initialized ==>error
   //list1.add(2);
+  list1.addAll(['Seth', 'Kathy', 'Lars']);
+  print(list1);
 }
